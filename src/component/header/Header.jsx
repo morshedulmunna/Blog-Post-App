75165fb92082d48f82de2216efefdee6429a8335
morshedulmunna <morshedulmunna1@gmail.com>
2022-03-18 09:05:25 +0600

    Header Added

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="text__part"></div>
+    </div>
+  );
+};
+
+export default Header;
