15cc313b46c72ba4c7d980528d0dea665edbc0a4
morshedulmunna <morshedulmunna1@gmail.com>
2022-03-20 13:24:39 +0600

    Login Page Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,16 @@ import Footer from "./component/footer/Footer";
 import Navbar from "./component/navbar/Navbar";
 import CreateBlog from "./pages/createBlog/CreateBlog";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+
         <Route path="/details" element={<CreateBlog />} />
       </Routes>
       <Footer />
